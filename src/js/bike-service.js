b4734f4b9b2db403cc69db1ca0aad6d9e5343a26
Vanessa Stewart <vstewart1213@gmail.com>
2020-09-10 11:44:35 -0700

    Add asyn function logic

--- src/js/bike-service.js
+++ src/js/bike-service.js
@@ -1,15 +1,32 @@
+// ASYNC and AWAIT
+
 export default class BikeService {
-  static getBikeInfo(location, distance) {
-    return fetch(`https://bikeindex.org:443/api/v3/search?page=1&per_page=25&location=${location}&distance=${distance}&stolenness=stolen`)
-      .then(function(response){
-        if (!response.ok) {
-          throw Error(response.status);
-        }
-        return response.json();
-      })
-      .catch(function(error) {
-        return error;
-      })
+  static async getBikeInfo(location,distance) {
+    try {
+      const response = await fetch(`https://bikeindex.org:443/api/v3/search?page=1&per_page=25&location=${location}&distance=${distance}&stolenness=stolen`);
+      if (!response.ok) {
+        throw Error(response.status)
+      }
+     return response.json();
+    } catch(error) {
+      return error.message
+    }
   }
 }
+
+// FETCH API METHOD
+// export default class BikeService {
+//   static getBikeInfo(location, distance) {
+//     return fetch(`https://bikeindex.org:443/api/v3/search?page=1&per_page=25&location=${location}&distance=${distance}&stolenness=stolen`)
+//       .then(function(response){
+//         if (!response.ok) {
+//           throw Error(response.status);
+//         }
+//         return response.json();
+//       })
+//       .catch(function(error) {
+//         return error;
+//       })
+//   }
+// }
 
--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import BikeService from './js/bike-Service.js';
 
-
+// ASYNC and AWAIT
 function getElements(response) {
   if (response.bikes) {
     response.bikes.forEach(function(bike){
@@ -15,15 +15,42 @@ function getElements(response) {
   }
 }
 
+async function makeApiCall(zip, distance){
+  const response = await BikeService.getBikeInfo(zip, distance);
+  getElements(response);
+}
 
 $(document).ready(function(){
   $("#searchButton").click(function(event) {
     event.preventDefault();
     let zip = $("#zipCode").val();
     let distance = $("#distance").val(); 
-    BikeService.getBikeInfo(zip,distance)  
-      .then(function(response){
-        getElements(response);
-      })
+    makeApiCall(zip, distance);
   });
 });
+
+
+
+
+// FETCH API METHOD
+// function getElements(response) {
+//   if (response.bikes) {
+//     response.bikes.forEach(function(bike){
+//       $(".showBikes").append(`<div>${bike.title}</div>`)
+//     })
+//   } else {
+//     $(".showErrors").text(`There was an error: ${response.message}`)
+//   }
+// }
+
+// $(document).ready(function(){
+//   $("#searchButton").click(function(event) {
+//     event.preventDefault();
+//     let zip = $("#zipCode").val();
+//     let distance = $("#distance").val(); 
+//     BikeService.getBikeInfo(zip,distance)  
+//       .then(function(response){
+//         getElements(response);
+//       })
+//   });
+// });
