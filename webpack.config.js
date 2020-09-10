1b85268d9bf7db2766b0c8503cc923750a990383
Vanessa Stewart <vstewart1213@gmail.com>
2020-09-10 09:12:11 -0700

    Add initial environment file tree

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,7 @@ module.exports = {
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      title: 'project',
+      title: 'bike-api',
       template: './src/index.html',
       inject: 'body'
     }),
